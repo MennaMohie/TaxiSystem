36d5aba056fa6cf322dca35e702d6894f73cfd9b
Yussuf Tarek <yussuftarek@outlook.com>
2017-12-17 17:17:20 +0200

    ..

--- Taxi Management System/Client.cs
+++ Taxi Management System/Client.cs
@@ -27,6 +27,8 @@ namespace Taxi_Management_System
         public string ClientName;
         public string ClientID;
         public string ClientPassword;
+
+        //khaleha vector
         Dictionary<string, ClientTrip> ClientTrip_ = new Dictionary<string, ClientTrip>();
 
 
@@ -34,88 +36,26 @@ namespace Taxi_Management_System
         //Filling the map of clients
         public bool FillCientMap()
         {
-            char[] Delimeters = { ' ', '.', ',', ':', '\t' };
-            StreamReader Stream = new StreamReader("Clients.txt");
-            string Line = Stream.ReadLine();
-
-            while (Line != null)
-            {
-                Client TempClient = new Client();
-                string[] Words = Line.Split(Delimeters);
-                TempClient.ClientUsername = Words[0];
-                TempClient.ClientName = Words[1];
-                TempClient.ClientID = Words[2];
-                TempClient.ClientPassword = Words[3];
-
-
-                //Sending the attributes to the MAP, Key -> USERNAME , Value -> Object of client Class
-                ClientGlobals.ClientMap.Add(TempClient.ClientUsername, TempClient);
-                Line = Stream.ReadLine();
-            }
             return true;
         }
 
         //Client Registeration
         public void ClientRegister()
         {
-            Client NewClient = new Client();
-            Console.WriteLine("Please enter your username: ");
-            NewClient.ClientUsername = Console.ReadLine();
-            Console.WriteLine("Please enter your password: ");
-            NewClient.ClientPassword = Console.ReadLine();
-            Console.WriteLine("Please enter your ID: ");
-            NewClient.ClientID = Console.ReadLine();
-            Console.WriteLine("Please enter your first name: ");
-            NewClient.ClientName = Console.ReadLine();
-            ClientGlobals.ClientMap.Add(NewClient.ClientUsername, NewClient);
-            Console.WriteLine("Registeration is done!");
-
-            //Fadel eny a3adel fel file bel map
         }
 
         //Login Function for the client
         public void ClientLogin(string uname, string password)
         {
-            if (ClientGlobals.ClientMap[uname].ClientPassword == password)
-            {
-                Console.WriteLine("You are now logged in.");
-            }
-            else
-            {
-                Console.WriteLine("Username or Password incorrect.");
-            }
         }
 
         //Reserving a taxi
         public void ReserveTaxi(Client Sender)
         {
-            ClientTrip TempTrip = new ClientTrip();
-            bool notFree = true;
-            while (notFree)
-            {
-                if (DriverGlobals.FreeDrivers.Peek().Status == "1")
-                {
-                    Console.WriteLine("Reservation is done!");
-                    Console.WriteLine("The Driver Name: " + DriverGlobals.FreeDrivers.Peek().DriverName);
-                    DriverGlobals.FreeDrivers.Peek().RecieveRequest(DriverGlobals.FreeDrivers.Peek(), Sender);
-                    TempTrip.DriverName = DriverGlobals.FreeDrivers.Peek().DriverName;
-                    TempTrip.ClientTripID = DriverGlobals.FreeDrivers.Peek().DriverID;
-                    TempTrip.From = "hakhodha mn l textbox";
-                    TempTrip.To = "hakhodha mn l textbox ely tahteeeh";
-                    ClientTrip_.Add(Sender.ClientUsername, TempTrip);
-                    notFree = false;
-                    break;
-                }
-                else
-                {
-                    DriverGlobals.FreeDrivers.Dequeue();
-                }
-            }
         }
 
         public void ViewClientHistory()
         {
-
         }
     }
 }
--- Taxi Management System/Driver.cs
+++ Taxi Management System/Driver.cs
@@ -38,89 +38,22 @@ namespace Taxi_Management_System
         //Filling the map of drivers
         public bool FillDriverMap()
         {
-            char[] Delimeters = { ' ', '.', ',', ':', '\t' };
-            StreamReader Stream = new StreamReader("Drivers.txt");
-            string Line = Stream.ReadLine();
-
-            while (Line != null)
-            {
-                string[] Words = Line.Split(Delimeters);
-
-                //Filling the Object of Driver Class
-                Driver TempDriver = new Driver();
-                TempDriver.DriverUsername = Words[0];
-                TempDriver.DriverID = Words[1];
-                TempDriver.DriverName = Words[2];
-                TempDriver.CarID = Words[3];
-                TempDriver.Salary = Words[4];
-                TempDriver.Status = Words[5];
-                TempDriver.DriverPassword = Words[6];
-
-                //Filling the free drivers in a queue
-                if (TempDriver.Status == "1")
-                {
-                    DriverGlobals.FreeDrivers.Enqueue(TempDriver);
-                }
-
-                //Sending the attributes to the MAP, Key -> USERNAME , Value -> Object of Driver Class
-                DriverGlobals.DriverMap.Add(TempDriver.DriverUsername, TempDriver);
-                Line = Stream.ReadLine();
-
-            }
-            Console.WriteLine("Driver Map is filled successfully");
             return true;
         }
 
         //Login Function for the driver
         public void DriverLogin(string uname, string password)
         {
-            if (DriverGlobals.DriverMap[uname].DriverPassword == password)
-            {
-                Console.WriteLine("You are now logged in.");
-            }
-            else
-            {
-                Console.WriteLine("Username or Password incorrect.");
-            }
         }
         //Function to change the Driver Status
 
         public void ChangeStatus(string name)
         {
-            //Checking if the map contains this username
-
-            if (DriverGlobals.DriverMap.ContainsKey(name))
-            {
-                //Changing his status
-
-                if (DriverGlobals.DriverMap[name].Status == "0")
-                {
-                    DriverGlobals.DriverMap[name].Status = "1";
-
-                    //Enqueue that free driver in the FreeDrivers queue
-                    DriverGlobals.FreeDrivers.Enqueue(DriverGlobals.DriverMap[name]);
-                }
-                if (DriverGlobals.DriverMap[name].Status == "1")
-                {
-                    DriverGlobals.DriverMap[name].Status = "0";
-                }
-            }
-            else
-            {
-                Console.WriteLine("Username not found.");
-            }
         }
 
         //Recieving a request
         public void RecieveRequest(Driver FreeDriver_, Client Customer)
         {
-            DriverTrip TempTrip = new DriverTrip();
-            FreeDriver_.ChangeStatus(FreeDriver_.DriverUsername);
-            TempTrip.ClientName = Customer.ClientName;
-            TempTrip.DriverTripID = Customer.ClientID;
-            TempTrip.From = "hakhodha mn textbox";
-            TempTrip.To = "hakhodha mn textbox ely taht ely foo'";
-            DriverTrip_.Add(FreeDriver_.DriverUsername, TempTrip);
         }
 
     }
