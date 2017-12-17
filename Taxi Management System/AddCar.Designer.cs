f8f3303eb800af2afd82ead0982a91e4d5dfd185
mennaawad <mennamawad16@gmail.com>
2017-12-17 21:18:25 +0200

    adding background to register

--- Taxi Management System/AddCar.Designer.cs
+++ Taxi Management System/AddCar.Designer.cs
@@ -53,9 +53,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(138, 165);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 19);
+            this.label2.Size = new System.Drawing.Size(76, 19);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Color :";
+            this.label2.Text = "COLOR :";
             // 
             // label3
             // 
--- Taxi Management System/Register.Designer.cs
+++ Taxi Management System/Register.Designer.cs
@@ -129,6 +129,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Taxi_Management_System.Properties.Resources.LogInBackground;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(584, 461);
             this.Controls.Add(this.signup);
