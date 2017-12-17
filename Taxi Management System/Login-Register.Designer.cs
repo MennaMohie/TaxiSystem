f92aadb759b170d2db3e26b163377ba1f2dbca7b
Olaadel <olaadel3@gmail.com>
2017-12-17 14:49:35 +0200

    LogIn -Register form finished.

--- Taxi Management System/Login-Register.Designer.cs
+++ Taxi Management System/Login-Register.Designer.cs
@@ -28,11 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LogInButton = new System.Windows.Forms.Button();
+            this.SignUpButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LogInButton
+            // 
+            this.LogInButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LogInButton.FlatAppearance.BorderSize = 0;
+            this.LogInButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LogInButton.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogInButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LogInButton.Location = new System.Drawing.Point(407, 169);
+            this.LogInButton.Name = "LogInButton";
+            this.LogInButton.Size = new System.Drawing.Size(118, 56);
+            this.LogInButton.TabIndex = 0;
+            this.LogInButton.Text = "Log In";
+            this.LogInButton.UseVisualStyleBackColor = false;
+            // 
+            // SignUpButton
+            // 
+            this.SignUpButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.SignUpButton.FlatAppearance.BorderSize = 0;
+            this.SignUpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SignUpButton.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SignUpButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.SignUpButton.Location = new System.Drawing.Point(407, 260);
+            this.SignUpButton.Name = "SignUpButton";
+            this.SignUpButton.Size = new System.Drawing.Size(118, 56);
+            this.SignUpButton.TabIndex = 1;
+            this.SignUpButton.Text = "Sign Up";
+            this.SignUpButton.UseVisualStyleBackColor = false;
+            // 
+            // Login_Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Taxi_Management_System.Properties.Resources.Login_Register_Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.SignUpButton);
+            this.Controls.Add(this.LogInButton);
+            this.Name = "Login_Register";
             this.Text = "Login_Register";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button LogInButton;
+        private System.Windows.Forms.Button SignUpButton;
     }
 }
