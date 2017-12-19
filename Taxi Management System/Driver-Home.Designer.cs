995663533aae031e38b0bdf3fcc7fac089798702
MennaMohie <mennaxmohiex@gmail.com>
2017-12-19 12:27:54 +0200

    Made some design changes.

--- Taxi Management System/Driver-Home.Designer.cs
+++ Taxi Management System/Driver-Home.Designer.cs
@@ -91,7 +91,6 @@
             this.Controls.Add(this.buttonDriverChangeStatus);
             this.Controls.Add(this.buttonDriverViewTrips);
             this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Driver_Home";
